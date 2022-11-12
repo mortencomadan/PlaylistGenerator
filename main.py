f6fd0964aedc07bb578b745ebea2207675ec39dc
Morten Hedegaard <morten@comadan.com>
2022-11-12 20:14:41 +0100

    cleaned up code

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import spotify
 
-
 with open("bands.txt") as file:
     lines = [line.rstrip() for line in file]
 
@@ -16,11 +15,3 @@ for line in lines:
     print()
 
 spotify.create_playlist("ArcTanGent2023", song_list)
-
-exit()
-
-
-
-
-
-#['6QN0mXlZOwfQoFs5yu55Pg', '79xLxZrpdURjzJmdHKB85a', '6DUvpC2bFViefEhZ65NGgk', '5gXKwH68RrWzpf6DdPa3vM', '6fcCbjJDY2nJBXZccXGMi5',
--- spotify.py
+++ spotify.py
@@ -1,13 +1,11 @@
 from difflib import SequenceMatcher
 from dotenv import load_dotenv
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyOAuth
 
 load_dotenv()
 scope = "playlist-modify-public"
 spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-
 
 
 class Song:
@@ -52,18 +50,18 @@ def find_band_with_song(bands, name_of_first_song):
         band.id = band_obj["id"]
 
         song_name = find_top_5(band).top_5_songs
-        #print(song_name[0].name, name_of_first_song)
+
         for song in song_name:
             if song.name.lower() == name_of_first_song.lower():
                 return band_obj
 
 
 def find_band(name_of_band, find_top_5_songs=False):
+    band = Band()
+    song_name = ""
+    multiple_matches = []
 
     split = name_of_band.split("|")
-
-    band_name = ""
-    song_name = ""
 
     if len(split) > 1:
         band_name = split[0]
@@ -71,21 +69,14 @@ def find_band(name_of_band, find_top_5_songs=False):
     else:
         band_name = name_of_band
 
-    band = Band()
-    multiple_matches = []
-
     results = spotify.search(q="artist: " + band_name, type="artist", limit=30)
-
     items = results["artists"]["items"]
 
     if len(items):
-        found_band = None
         best_ratio = 0
 
         for bands in items:
-            #print(bands["name"])
             ratio = similar(bands["name"].lower(), band_name.lower())
-            #print(ratio)
 
             if ratio > best_ratio:
                 best_ratio = ratio
@@ -93,20 +84,16 @@ def find_band(name_of_band, find_top_5_songs=False):
             if ratio == 1.0:
                 multiple_matches.append(bands)
 
-
-
         if len(multiple_matches) > 1 and not song_name:
             print("There was multiple results with same name:", band_name)
 
         if song_name:
             found_band = find_band_with_song(multiple_matches, song_name)
 
-        #print(found_band)
-
-
         band.genres = found_band["genres"]
         band.name = found_band["name"]
         band.id = found_band["id"]
+
     else:
         print("could not find:" + name_of_band)
 
@@ -123,4 +110,3 @@ def create_playlist(playlist_name, song_list):
     playlist = spotify.user_playlist_create(user_id, playlist_name)
     for chunk in song_list_split:
         spotify.playlist_add_items(playlist["id"], chunk)
-    #3spotify.current_user_unfollow_playlist(playlist["id"])
